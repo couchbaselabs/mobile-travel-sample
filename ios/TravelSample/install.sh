9e78d934e545856f3998e5f2b51d32ff1ef24026
James Nocentini <james.nocentini@gmail.com>
2017-10-04 13:32:03 +0200

    update download URL to point to DB15. There are compile time errors with DB16.

--- ios/TravelSample/install.sh
+++ ios/TravelSample/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd Frameworks
-curl https://packages.couchbase.com/releases/couchbase-lite/ios/2.0DB16/couchbase-lite-swift_community_2.0.0-392.zip > cbl.zip
+curl https://packages.couchbase.com/releases/couchbase-lite/ios/2.0DB15/couchbase-lite-swift_community_2.0.0-375.zip > cbl.zip
 unzip -n cbl.zip
 rm -rf cbl.zip
 rm -rf cbl
