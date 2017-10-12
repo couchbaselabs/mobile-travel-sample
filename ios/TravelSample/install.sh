9c5d054cab16533f99ef65994cb255ce1925d93c
James Nocentini <james.nocentini@gmail.com>
2017-10-12 12:48:53 +0100

    use packages.couchbase.com link to download DB18

--- ios/TravelSample/install.sh
+++ ios/TravelSample/install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 cd Frameworks
-#curl https://packages.couchbase.com/releases/couchbase-lite/ios/2.0DB16/couchbase-lite-swift_community_2.0.0-392.zip > cbl.zip
-curl http://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-lite-ios/master/433/couchbase-lite-swift_community_2.0.0-433.zip > cbl.zip
+curl https://packages.couchbase.com/releases/couchbase-lite/ios/2.0DB18/couchbase-lite-swift_community_2.0.0-434.zip > cbl.zip
 unzip -n cbl.zip
 rm -rf cbl.zip
 rm -rf cbl
