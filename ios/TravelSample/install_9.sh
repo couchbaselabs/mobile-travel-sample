2ff5758f59f073b88a16f13cc73fd092c308c06c
rajagp <r7.priya@gmail.com>
2017-10-16 09:26:50 -0400

    Separated install.sh to install_8.sh for xcode 8 and install_9.sh for xcode9. Updated content install guide. Also placeholder images for rightscale install

--- ios/TravelSample/install_9.sh
+++ ios/TravelSample/install_9.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# This downloads swift 3.2/ swift4 compatible version of CBL. Use with Xcode 9
 cd Frameworks
 curl https://packages.couchbase.com/releases/couchbase-lite/ios/2.0DB18/couchbase-lite-swift_community_2.0.0-434.zip > cbl.zip
 unzip -n cbl.zip
