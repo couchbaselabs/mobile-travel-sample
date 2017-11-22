e600aa0383e0b019b607b64c95d5228afc15f59e
rajagp <r7.priya@gmail.com>
2017-11-22 10:21:31 -0500

    Updated install scripts for DB20

--- ios/TravelSample/install_8.sh
+++ ios/TravelSample/install_8.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # This downloads swift 3.1 compatible version of CBL (Use with Xcode 8)
 cd Frameworks
+rm -rf iOS
+rm -rf macOS
+rm -rf tvOS
 curl http://couchbase-docs.s3.amazonaws.com/workshop/db20-xcode8/couchbase-lite-swift_db20_xcode8_community.zip > cbl.zip
-unzip -n cbl.zip
+unzip -n cbl.zip -d iOS
 rm -rf cbl.zip
-rm -rf cbl
--- ios/TravelSample/install_9.sh
+++ ios/TravelSample/install_9.sh
@@ -2,6 +2,9 @@
 
 # This downloads swift 3.2/ swift4 compatible version of CBL. Use with Xcode 9
 cd Frameworks
+rm -rf iOS
+rm -rf macOS
+rm -rf tvOS
 curl https://packages.couchbase.com/releases/couchbase-lite/ios/2.0DB20/couchbase-lite-swift_community_2.0DB20.zip > cbl.zip
 unzip -n cbl.zip
 rm -rf cbl.zip
