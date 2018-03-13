da42d13518efd880805cd4e53036d24818a72284
rajagp <r7.priya@gmail.com>
2018-03-12 22:30:24 -0400

    Android app: removed unused code

--- android/app/src/main/java/com/couchbase/travelsample/login/Login.java
+++ android/app/src/main/java/com/couchbase/travelsample/login/Login.java
@@ -26,29 +26,6 @@ public class Login extends AppCompatActivity  {
 
     EditText usernameInput;
     EditText passwordInput;
-    // Minimum acceleration needed to count as a shake movement
-    private static final int MIN_SHAKE_ACCELERATION = 5;
-
-    // Minimum number of movements to register a shake
-    private static final int MIN_MOVEMENTS = 2;
-
-    // Maximum time (in milliseconds) for the whole shake to occur
-    private static final int MAX_SHAKE_DURATION = 10000;
-
-    // Arrays to store gravity and linear acceleration values
-    private float[] mGravity = { 0.0f, 0.0f, 0.0f };
-    private float[] mLinearAcceleration = { 0.0f, 0.0f, 0.0f };
-
-    // Indexes for x, y, and z values
-    private static final int X = 0;
-    private static final int Y = 1;
-    private static final int Z = 2;
-    private SensorManager mSensorManager;
-    // Start time for the shake detection
-    long startTime = 0;
-
-    // Counter for shake movements
-    int moveCount = 0;
 
 
     @Override
@@ -60,8 +37,6 @@ public class Login extends AppCompatActivity  {
 
         usernameInput = (EditText) findViewById(R.id.usernameInput);
         passwordInput = (EditText) findViewById(R.id.passwordInput);
-        /* do this in onCreate */
-        mSensorManager = (SensorManager) getSystemService(getApplicationContext().SENSOR_SERVICE);
 
 
     }
@@ -69,8 +44,6 @@ public class Login extends AppCompatActivity  {
     @Override
     protected void onStart() {
         super.onStart();
-        // Shake Gesture
-         mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
 
     }
 
@@ -93,111 +66,6 @@ public class Login extends AppCompatActivity  {
     }
 
 
-    private final SensorEventListener mSensorListener = new SensorEventListener() {
-
-        public void onSensorChanged(SensorEvent se) {
-
-            // This method will be called when the accelerometer detects a change.
-
-            // Call a helper method that wraps code from the Android developer site
-            setCurrentAcceleration(se);
-
-            // Get the max linear acceleration in any direction
-            float maxLinearAcceleration = getMaxCurrentLinearAcceleration();
-
-            // Check if the acceleration is greater than our minimum threshold
-            if (maxLinearAcceleration > MIN_SHAKE_ACCELERATION) {
-                long now = System.currentTimeMillis();
-
-                // Set the startTime if it was reset to zero
-                if (startTime == 0) {
-                    startTime = now;
-                }
-
-                long elapsedTime = now - startTime;
-
-                // Check if we're still in the shake window we defined
-                if (elapsedTime > MAX_SHAKE_DURATION) {
-                    // Too much time has passed. Start over!
-                    resetShakeDetection();
-                }
-                else {
-                    // Keep track of all the movements
-                    moveCount++;
-
-                    // Check if enough movements have been made to qualify as a shake
-                    if (moveCount > MIN_MOVEMENTS) {
-                        // It's a shake! Show an alert to accept SGW and web server endpoint
-
-                        // Reset for the next one!
-                        resetShakeDetection();
-                 //       showAlertForAddressEntry();
-
-
-                    }
-                }
-            }
-
-        }
-
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        }
-
-        // Shake Gesture Recognition Related
-        private void setCurrentAcceleration(SensorEvent event) {
-        /*
-         *  BEGIN SECTION from Android developer site. This code accounts for
-         *  gravity using a high-pass filter
-         */
-
-            // alpha is calculated as t / (t + dT)
-            // with t, the low-pass filter's time-constant
-            // and dT, the event delivery rate
-
-            final float alpha = 0.8f;
-
-            // Gravity components of x, y, and z acceleration
-            mGravity[X] = alpha * mGravity[X] + (1 - alpha) * event.values[X];
-            mGravity[Y] = alpha * mGravity[Y] + (1 - alpha) * event.values[Y];
-            mGravity[Z] = alpha * mGravity[Z] + (1 - alpha) * event.values[Z];
-
-            // Linear acceleration along the x, y, and z axes (gravity effects removed)
-            mLinearAcceleration[X] = event.values[X] - mGravity[X];
-            mLinearAcceleration[Y] = event.values[Y] - mGravity[Y];
-            mLinearAcceleration[Z] = event.values[Z] - mGravity[Z];
-
-        /*
-         *  END SECTION from Android developer site
-         */
-        }
-
-        private float getMaxCurrentLinearAcceleration() {
-            // Start by setting the value to the x value
-            float maxLinearAcceleration = mLinearAcceleration[X];
-
-            // Check if the y value is greater
-            if (mLinearAcceleration[Y] > maxLinearAcceleration) {
-                maxLinearAcceleration = mLinearAcceleration[Y];
-            }
-
-            // Check if the z value is greater
-            if (mLinearAcceleration[Z] > maxLinearAcceleration) {
-                maxLinearAcceleration = mLinearAcceleration[Z];
-            }
-
-            // Return the greatest value
-            return maxLinearAcceleration;
-        }
-
-        private void resetShakeDetection() {
-            startTime = 0;
-            moveCount = 0;
-        }
-
-
-
-    };
-
     @Override
     protected void onResume() {
         super.onResume();
@@ -206,7 +74,6 @@ public class Login extends AppCompatActivity  {
 
     @Override
     protected void onPause() {
-        mSensorManager.unregisterListener(mSensorListener);
         super.onPause();
     }
 
