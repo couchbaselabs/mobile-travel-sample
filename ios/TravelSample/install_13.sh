f1fba9fb4ef696a36fc421d0bf337ad61236eca4
Vlad Velicu <vlad.velicu@couchbase.com>
2022-10-25 12:30:05 +0100

    updates per feedback

--- ios/TravelSample/install_13.sh
+++ ios/TravelSample/install_13.sh
@@ -5,7 +5,7 @@ cd Frameworks
 rm -rf iOS
 rm -rf macOS
 rm -rf tvOS
-curl https://packages.couchbase.com/releases/couchbase-lite-ios/3.0.2/couchbase-lite-swift_xc_enterprise_3.0.2co.zip > cbl.zip
+curl https://packages.couchbase.com/releases/couchbase-lite-ios/3.0.2/couchbase-lite-swift_xc_enterprise_3.0.2.zip > cbl.zip
 unzip -n cbl.zip
 rm -rf cbl.zip
 rm -rf cbl
